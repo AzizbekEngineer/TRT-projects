b25c6869c8e565c10cca7a7653b3960fd9765ca4
AzizbekEngineer <azizbekengineer@gmail.com>
2025-01-19 11:31:41 +0500

    add hero

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-// import Hero from "../../components/hero/Hero";
+import Hero from "../../components/hero/Hero";
 import Auto from "../../components/auto/Auto";
 import Prinsp from "../../components/prinsp/Prinsp";
 import Vapros from "../../components/vapros/Vapros";
@@ -9,7 +9,7 @@ import Catalog from "../../components/katalog/Catalog";
 const Home = () => {
   return (
     <Fragment>
-      {/* <Hero /> */}
+      <Hero />
       <Filter />
       <Auto />
       <Prinsp />
